63d94b5b248d73cb67f67e0155df798e5645495c
Landon-Bondurant <64943358+Landon-Bondurant@users.noreply.github.com>
2026-03-25 23:55:19 -0600

    do ica11

---
+++ ica/ica11/ica11.js
@@ -0,0 +1,50 @@
+// Complete variable definitions and random functions
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// Raw text strings
+
+let names = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+let places = ["the soup kitchen", "Disneyland", "the White House"];
+
+let acts = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+
+// Partial return random string function
+
+function returnRandomStoryString() {
+  let randomName = randomValueFromArray(names);
+  let randomPlace = randomValueFromArray(places);
+  let randomAct = randomValueFromArray(acts);
+
+  let storyText = `It was 94 Fahrenheit outside, so ${randomName} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomAct}. Bob saw the whole thing, but was not surprised — ${randomName} weighs 300 pounds, and it was a hot day.`;
+
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  if (customName.value !== "") {
+    const name = customName.value;
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
+  }
+
+  // TODO: replace "" with the correct expression
+  story.textContent = returnRandomStoryString();
+  // placeholder text: "hat alakazam aaaaaaaaaaaaaaaaaaaa hat alakazam aaaaaaaaaaaaaaaaaaaahat alakazam aaaaaaaaaaaaaaaaaaaa";
+  story.style.visibility = "visible";
+}
