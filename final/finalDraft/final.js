29e3019f10e11085cbba0a57fefe0ce88b750826
Landon-Bondurant <64943358+Landon-Bondurant@users.noreply.github.com>
2026-04-29 17:17:19 -0600

    Set up final draft version

---
+++ final/finalDraft/final.js
@@ -0,0 +1,83 @@
+let volume = 0;
+const volSlider = document.getElementById("volume-slider");
+const volDisplay = document.getElementById("volDisplay");
+const volDisp = document.getElementById("volumeDisp");
+const seal = document.getElementById("seal");
+const unseal = document.getElementById("unseal");
+const cl = document.getElementById("close");
+const op = document.getElementById("open");
+const vslabel = document.getElementById("vslabel");
+const vdlabel = document.getElementById("vdlabel");
+const vdslabel = document.getElementById("vdsplabel");
+const br1 = document.getElementById("br1");
+const br2 = document.getElementById("br2");
+const ss = document.getElementById("sealStatus");
+let valPrev = 0;
+let valNext = 0;
+let delta = 0;
+let prevt = 0;
+let sealed = false;
+seal.addEventListener("click", function() {
+    sealed = true;
+    ss.innerText = "Volume: sealed";
+    alert("Volume set to " + Math.ceil(volume) + "!");
+});
+unseal.addEventListener("click", function() {
+    sealed = false;
+    ss.innerText = "Volume: unsealed";
+});
+cl.addEventListener("click", function() {
+    volSlider.style.display = "none";
+    volDisplay.style.display = "none";
+    volDisp.style.display = "none";
+    seal.style.display = "none";
+    unseal.style.display = "none";
+    cl.style.display = "none";
+    op.style.display = "inline-block";
+    vslabel.style.display = "none";
+    vdlabel.style.display = "none";
+    vdslabel.style.display = "none";
+    br1.style.display = "none";
+    br2.style.display = "none";
+});
+op.addEventListener("click", function() {
+    volSlider.style.display = "";
+    volDisplay.style.display = "";
+    volDisp.style.display = "";
+    seal.style.display = "";
+    unseal.style.display = "";
+    cl.style.display = "";
+    op.style.display = "";
+    vslabel.style.display = "";
+    vdlabel.style.display = "";
+    vdslabel.style.display = "";
+    br1.style.display = "";
+    br2.style.display = "";
+});
+function loop(ct) {
+    valNext = volSlider.value;
+    delta = valNext - valPrev;
+    let dt = ct - prevt;
+    prevt = ct
+    if (!sealed) {
+        volume -= 0.2 * dt / 50;
+        if (delta < 0) {
+            volume += (-delta) / 30;
+        }
+        if (volume < 0) {
+            volume = 0;
+        }
+        else if (volume > 100) {
+            volume = 100;
+        }
+    }
+        //console.log(document.getElementById("a").value);
+    volDisplay.innerText = Math.ceil(volume);
+    volDisp.value = volume;
+    valPrev = valNext;
+
+
+
+    requestAnimationFrame(loop);
+}
+requestAnimationFrame(loop);
